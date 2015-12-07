58c3e41ad02783af00bfe612f01ccb7a32636d7a
Ian Stewart <iastewar555@gmail.com>
2015-12-07 14:16:20 -0800

    now supports all file types

--- app/javascripts/index.js
+++ app/javascripts/index.js
@@ -9,19 +9,21 @@ var watcher;
 var connectedDirectory;
 var serverDirectory;
 
-// ask the server to be assigned to a room. This way only clients in the same room
-// will be able to receive file changes.
-var requestRoom = function(directoryName) {
-  var dirArray = directoryName.split("/");
-  var currentDir = dirArray[dirArray.length-1]
-  socket.emit('request room', currentDir);
+// converts an ArrayBuffer to a Buffer
+var toBuffer = function(ab) {
+    var buffer = new Buffer(ab.byteLength);
+    var view = new Uint8Array(ab);
+    for (var i = 0; i < buffer.length; ++i) {
+        buffer[i] = view[i];
+    }
+    return buffer;
 }
 
 // sends a file to the server where directoryName/fileName is the path of the file,
 // and data is the content of the file
 var sendFileToServer = function(directoryName, fileName, data) {
   var dirArray = directoryName.split("/");
-  var currentDir = dirArray[dirArray.length-1]
+  var currentDir = dirArray[dirArray.length-1];
   socket.emit('send file', {fileName: currentDir + '/' + fileName, fileContents: data});
 
 }
@@ -29,7 +31,7 @@ var sendFileToServer = function(directoryName, fileName, data) {
 // deletes a file from the server where directoryName/fileName is the path of the file,
 var deleteFileFromServer = function(directoryName, fileName) {
   var dirArray = directoryName.split("/");
-  var currentDir = dirArray[dirArray.length - 1]
+  var currentDir = dirArray[dirArray.length - 1];
   socket.emit('send file', {fileName: currentDir + '/' + fileName, deleted: true});
 }
 
@@ -38,7 +40,7 @@ var sendDirectory = function(directoryName, subDirectories) {
     if (err) throw err;
     // fileName could be a file or a directory
     fileNames.forEach(function(fileName){
-      fs.readFile(directoryName + '/' + subDirectories + '/' + fileName, 'utf-8', function(err, data) {
+      fs.readFile(directoryName + '/' + subDirectories + '/' + fileName, function(err, data) {
         var subDirs;
         if (subDirectories === "") {
           subDirs = fileName;
@@ -72,12 +74,10 @@ $(function() {
       } else {
         $("#log").append("<div>Listening to " + directoryNames[0] + "</div>");
 
-        requestRoom(directoryNames[0]);
-
         sendDirectory(directoryNames[0], "");
 
         watcher = fs.watch(directoryNames[0], { persistent: true, recursive: true }, function(event, fileName) {
-          fs.readFile(directoryNames[0] + '/' + fileName, 'utf-8', function(err, data) {
+          fs.readFile(directoryNames[0] + '/' + fileName, function(err, data) {
             if (err) {
               // delete from server
               deleteFileFromServer(directoryNames[0], fileName);
@@ -85,11 +85,11 @@ $(function() {
               // send to server
               sendFileToServer(directoryNames[0], fileName, data);
             }
-          })
-        })
+          });
+        });
       }
-    })
-  })
+    });
+  });
 
   $("#stopListening").on("click", function() {
     if (watcher) {
@@ -97,7 +97,7 @@ $(function() {
       watcher = null;
       $("#log").append("<div>Stopped listening to folder</div>");
     }
-  })
+  });
 
   $("#connectFolder").on("click", function() {
     dialog.showOpenDialog({ properties: ['openDirectory']}, function(directoryNames) {
@@ -118,8 +118,8 @@ $(function() {
         $("#log").append("<div>Connected to: " + serverFolder + ", storing to " + directoryNames[0] + "</div>");
 
       }
-    })
-  })
+    });
+  });
 
   $("#disconnectFolder").on("click", function() {
     if (connectedDirectory && serverDirectory) {
@@ -128,7 +128,7 @@ $(function() {
       connectedDirectory = null;
       serverDirectory = null;
     }
-  })
+  });
 
   socket.on('send file', function(msg) {
     //$('#file').text(msg.fileName);
@@ -154,7 +154,7 @@ $(function() {
           });
         // otherwise, save the file
         } else {
-          fs.writeFile(connectedDirectory + '/' + msg.fileName, msg.fileContents, function(err) {
+          fs.writeFile(connectedDirectory + '/' + msg.fileName, toBuffer(msg.fileContents), function(err) {
               if(err) {
                   return console.log(err);
               }
@@ -169,9 +169,9 @@ $(function() {
   socket.on('folder does not exist', function() {
     connectedDirectory = null;
     serverDirectory = null;
-    ("#log").append("<div>Folder does not exit</div>")
-  })
+    ("#log").append("<div>Folder does not exit</div>");
+  });
 
 
 
-})
+});
