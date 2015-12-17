26788030a55bed1d51668b74db35de483b7283e3
Ian Stewart <iastewar555@gmail.com>
2015-12-16 18:18:46 -0800

    final ip

--- app/javascripts/serverURL.js
+++ app/javascripts/serverURL.js
@@ -1,5 +1,5 @@
 // the server url must also be changed in index.html, login.html, and signup.html
 
-var serverURL = 'http://52.34.2.212:3000';
+var serverURL = 'http://52.34.2.212';
 
 module.exports = serverURL;
